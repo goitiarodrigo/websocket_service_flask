c2a370b53315824af374f03889d479ad4b0be0ff
Rodrigo <rgoitia>
2023-06-25 11:27:08 -0300

    Added new requirement

--- app.py
+++ app.py
@@ -64,3 +64,6 @@ print(__name__, 'NAME!!')
 if __name__ == '__main__' :
     socketio.start_background_task(generate_alert_price)
     socketio.run(app, host='0.0.0.0', debug=True)
+if __name__ == 'app':
+    socketio.start_background_task(generate_alert_price)
+    socketio.run(app, host='0.0.0.0', debug=False)
